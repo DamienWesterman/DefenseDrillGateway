97a0fbf085565d867e7f46a75c6316971b932925
Damien Westerman <damien.westerman@snhu.edu>
2024-10-24 19:53:00 -0600

    Initial Repo Setup

---
+++ src/main/java/com/damienwesterman/defensedrill/gateway/DefenseDrillGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.damienwesterman.defensedrill.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefenseDrillGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefenseDrillGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damienwesterman/defensedrill/gateway/DefenseDrillGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damienwesterman.defensedrill.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefenseDrillGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

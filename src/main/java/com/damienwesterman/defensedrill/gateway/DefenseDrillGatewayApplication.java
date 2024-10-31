3c4e1977fe987f2220e55807fffba23e81555690
Damien Westerman <damien.westerman@snhu.edu>
2024-10-31 14:05:43 -0600

    Just enough to get the server to work

--- src/main/java/com/damienwesterman/defensedrill/gateway/DefenseDrillGatewayApplication.java
+++ src/main/java/com/damienwesterman/defensedrill/gateway/DefenseDrillGatewayApplication.java
@@ -2,9 +2,13 @@ package com.damienwesterman.defensedrill.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DefenseDrillGatewayApplication {
+	// TODO: Test to make sure that streaming video through this gateway works
+	// TODO: FIXME: do a filter or something to only allow connection to certain endpoints from local networks
 
 	public static void main(String[] args) {
 		SpringApplication.run(DefenseDrillGatewayApplication.class, args);
